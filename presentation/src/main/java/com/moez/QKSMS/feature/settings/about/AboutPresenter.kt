6f8586a07cf2b348a3f1d88e5574ea81fd6cf1b8
maksimnovikov <maksim@unplugged-systems.com>
2024-12-25 10:28:22 +0200

    UNP-3058 Rollback expand layout

--- presentation/src/main/java/com/moez/QKSMS/feature/settings/about/AboutPresenter.kt
+++ presentation/src/main/java/com/moez/QKSMS/feature/settings/about/AboutPresenter.kt
@@ -36,6 +36,8 @@ class AboutPresenter @Inject constructor(
                 .autoDisposable(view.scope())
                 .subscribe { preference ->
                     when (preference.id) {
+                        R.id.openSourceHeader -> view.toggleOpenSourceContentVisibility()
+
                         R.id.up_open_source_link -> navigator.showUpOpenSourceLink()
 
                         R.id.developer -> navigator.showDeveloper()
