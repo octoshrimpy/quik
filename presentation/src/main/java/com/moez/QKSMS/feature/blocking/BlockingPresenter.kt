93610d1603e966ae029d143849aac9308e4c14ee
Inhishonor <inhishonor@protonmail.com>
2025-05-20 13:34:56 -0700

    Rework Strings
    Removed some unneeded ones, and renamed others, the blocking manager string, was just QUIK, so why not use the app_name string?

--- presentation/src/main/java/com/moez/QKSMS/feature/blocking/BlockingPresenter.kt
+++ presentation/src/main/java/com/moez/QKSMS/feature/blocking/BlockingPresenter.kt
@@ -41,7 +41,7 @@ class BlockingPresenter @Inject constructor(
                         Preferences.BLOCKING_MANAGER_CB -> R.string.blocking_manager_call_blocker_title
                         Preferences.BLOCKING_MANAGER_CC -> R.string.blocking_manager_call_control_title
                         Preferences.BLOCKING_MANAGER_SIA -> R.string.blocking_manager_sia_title
-                        else -> R.string.blocking_manager_qksms_title
+                        else -> R.string.app_name
                     }
                 }
                 .map(context::getString)
